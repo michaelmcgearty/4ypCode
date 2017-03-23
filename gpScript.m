ae9d471de038bc38df63bb811b2deee2c068968c
Michael Mcgearty <mcgearty.m@gmail.com>
2017-03-23 12:58:09 +0000

    Moved the optimisation procedure into its own function to tidy gpScript, added error checking in cholInv

--- gpScript.m
+++ gpScript.m
@@ -1,46 +1,49 @@
+%% Clear Matlab before running
+
 % Close all figures and clear all variables from the workspace
 clear all; close all;
 
-% Load the data from csv
-run(loadData)
+%% Specification of task from user
 
-% Organise data, i denotes the collumn containing the output variable
-% dimX = size(X);
-% N = dimX(1);
-% D = dimX(2) - 1;
-% yd = X(:,i);
-% xd = [X(:,1:(i-1)),X(:,(i+1):D+1)]; 
-% OR 
-% xd = X(:,j);
-
-% Specification of task from user
 % Specify the type of mean and covariance functions being used
-type = struct('Mean',{'type of mean func goes here'},'Cov',{'type(s) of covariance func go here'});
-% Specify the initial value and upper bounds on the hyper parameters
-initVal = 1; %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Arbitrarily assigned 
-UB = 10; %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Arbitrarily assigned
+type = struct('Mean',{'meanZero'},'Cov',{'covSEard'});
+
+%Specify the variable of interest and the input variables
+varOut = 'varaible of interest goes here';
+varIn = struct('Name',{'input variable name(s) go here'});
+
+% Specify the initial value and upper bounds on the hyper parameters 
+initVal = 1;
+UB = 10;
+
+% Specify the maximum number of iterations and function tolerance for
+% optimisation
+maxIter = 500;
+funcTol = 1e-1;
+
+%% Load data and initialise variables
+
+% Load the data from csv
+[xd, yd, x_] = loadData(varOut, varIn);
 
 % Initialise the hyper parameters based on type.Mean and type.Cov
 thetaInit = hypInit(type, D, initVal);
 thetaLB = zeros(size(thethaInit));
 thetaUB = UB * ones(size(thetaInit)); 
 
+%% Optimisation
+
 % Opimise the hyper parameters by minimising the negative log likelihood
 % using the simulated annealing algorithm
+[thetaOpt, nLogLikMin, exitFlag, output] = optSimAn(type, xd, yd, ...
+    thetaInit, thetaLB, thetaUB, maxIter, funcTol);
 
-% Create an anonymous function to pass as the loss function wrt theta
-lossFunc = @(theta)gpLik(type, theta, xd, yd);
-% Specify additional settings
-options = optimoptions(@simulannealbnd,'FunctionTolerance',1e-1,...
-    'MaxIterations',500);
-% options = optimoptions(@simulannealbnd,'FunctionTolerance',1e-1,...
-%     'MaxIterations',500,'PlotFcn',{@saplotbestx,@saplotbestf,@saplotx,...
-%     @saplotf}); 
-%Calculate the optimal hyper parameter values
-[thetaOpt, nLogLikMin] = simulannealbnd(lossFunc,thetaInit,thetaLB,thetaUB,options);
+%% Regression
 
 % Make predictions using the GP
 [f_, f_Var] = gpReg(type, thetaOpt, xd, yd, x_);
+
+%% Plotting and saving
 
 % Generate a plot of data
 
---
+++ opt/optSimAn.m
@@ -0,0 +1,37 @@
+function [thetaOpt,nLogLikMin,exitFlag,output] = optSimAn(type,xd,yd,thetaInit,thetaLB,thetaUB,maxIter,funcTol)
+% optSimAn: Minimses the negative log likelihood with respect to the hyper
+%           parameters using the simulated anealing algorithm
+%
+% Inputs: - type - a structure specifying the type of mean and covariance
+%           functions being used
+%         - xd -the trainig data inputs in an N * D matrix
+%         - yd - the training targets in an N * 1 vector
+%         - thetaInit - the initial hyper parameter values
+%         - thetaLB - a lower bound on the possible hyper parameter values
+%         - thetaUB - an upper bound on the possible hyper parameter values
+%         - maxIter - the maximum number of iterations permitted before the 
+%           algorithm terminates
+%         - funcTol - termination tolerance on function value
+%
+% Outputs: - thetaOpt - the optimum values for the hyper parameters
+%          - nLogLikMin - the minimum value of the negative log likelihood
+%          - exitFlag - reason simulannealbnd stopped, returned as an integer
+%          - output - information about the optimization process, returned 
+%            as a structure
+
+% Create an anonymous function to pass as the loss function wrt theta
+lossFunc = @(theta)gpLik(type, theta, xd, yd);
+
+% Specify additional settings
+options = optimoptions(@simulannealbnd,'FunctionTolerance',funcTol,...
+    'MaxIterations',maxIter);
+
+% Extra plotting settings that might be usedful for debugging (can be
+% included in the options structure
+% 'PlotFcn',{@saplotbestx,@saplotbestf,@saplotx,@saplotf}
+
+%Calculate the optimal hyper parameter values
+[thetaOpt, nLogLikMin, exitFlag, output] = simulannealbnd(lossFunc,...
+    thetaInit,thetaLB,thetaUB,options);
+
+end
